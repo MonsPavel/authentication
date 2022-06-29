9b7fcfacd05da6c0201ca95a7ebc4e26d3cf3483
MonsPavel <44977810+MonsPavel@users.noreply.github.com>
2022-06-29 22:14:32 +0300

    body validation (#5)

--- router/index.js
+++ router/index.js
@@ -1,8 +1,9 @@
 const Router = require('express').Router;
 const router = new Router();
 const { registration, login, logout, activate, refresh, getUsers } = require('../controllers/user-controller');
+const { body } = require('express-validator');
 
-router.post('/register', registration);
+router.post('/register', body('email').isEmail(), registration);
 router.post('/login', login);
 router.post('/logout', logout);
 router.get('/activate/:link', activate);
