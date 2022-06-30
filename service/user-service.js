6b9ff410e10037c57e82db1711c853659053b29b
MonsPavel <44977810+MonsPavel@users.noreply.github.com>
2022-06-30 23:26:25 +0300

    login logic (#6)

--- service/user-service.js
+++ service/user-service.js
@@ -38,6 +38,25 @@ class UserService {
         user.isActivated = true;
         await user.save();
     }
+
+    async login(email, password) {
+        const user = await UserModal.findOne({ email });
+        if(!user) {
+            throw ApiError.BadRequest('Пользователь с таким email не найден')
+        }
+        const isPassEquals = await bcrypt.compare(password, user.password)
+        if(!isPassEquals) {
+            throw ApiError.BadRequest('Неверный пароль')
+        }
+        const userDto = new UserDto(user)
+        const tokens = generateTokens({ ...userDto })
+        await saveToken(userDto.id, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: UserDto
+        }
+    }
 }
 
 module.exports = new UserService();
